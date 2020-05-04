1a23eeaacfad7180efa5fd66f0b62d18d1c82145
Maksim Koshcheev <maxkoshe5@gmail.com>
2020-05-04 14:17:24 +0300

    calendar

--- Assets/Scripts/UI/ScrollRectSyncronizer.cs
+++ Assets/Scripts/UI/ScrollRectSyncronizer.cs
@@ -7,6 +7,7 @@ public class ScrollRectSyncronizer : MonoBehaviour
 	public RectTransform scrollRect;
 	public Scrollbar scrollbar;
 	public bool isVertical;
+	public float offset;
 
 	public Vector2 defaultAnchoredPosition;
 
@@ -22,13 +23,13 @@ public class ScrollRectSyncronizer : MonoBehaviour
 			this.rectTransform.anchoredPosition =
 				new Vector3(
 					this.rectTransform.anchoredPosition.x,
-					this.defaultAnchoredPosition.y + (1 - this.scrollbar.value) * this.scrollRect.sizeDelta.y);
+					this.defaultAnchoredPosition.y + (1 - this.scrollbar.value) * (this.scrollRect.sizeDelta.y - this.offset));
 		}
 		else
 		{
 			this.rectTransform.anchoredPosition =
 				new Vector3(
-					this.defaultAnchoredPosition.x - (this.scrollbar.value) * this.scrollRect.sizeDelta.x,
+					this.defaultAnchoredPosition.x - (this.scrollbar.value) * (this.scrollRect.sizeDelta.x - this.offset),
 					this.rectTransform.anchoredPosition.y);
 		}
 	}
