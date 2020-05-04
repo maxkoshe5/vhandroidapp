1a19a2886b3cbb84f99325c49e66e3edf851e6ca
Maksim Koshcheev <maxkoshe5@gmail.com>
2020-05-04 13:13:45 +0300

    v2

---
+++ Assets/Scripts/UI/ScrollRectSyncronizer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollRectSyncronizer : MonoBehaviour
+{
+	public RectTransform rectTransform;
+	public RectTransform scrollRect;
+	public Scrollbar scrollbar;
+
+	private void Update()
+	{
+		this.rectTransform.anchoredPosition = 
+			new Vector3(
+				this.rectTransform.position.x,
+				(1 - this.scrollbar.value) * this.scrollRect.sizeDelta.y,
+				this.rectTransform.position.z);
+	}
+}
